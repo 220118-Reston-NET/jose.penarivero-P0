145f3370a2a799e3bbd198c9ba6298e7a81ff7e3
josepena67 <jospenmuss@hotmail.com>
2022-01-27 09:58:18 -0500

    Some functionality

--- DrinkStore/DrinkUI/Program.cs
+++ DrinkStore/DrinkUI/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+ //using DrinkModel;
+ using DrinkUI;
+// Console.WriteLine("Hello, World!");
+// Inventory ab = new Inventory();
+// ab.DrinkType =  1;
+bool repeating =true;
+IUserMenu menu = new MainMenu();
+
+while(repeating)
+{
+Console.Clear();
+
+menu.Display();
+ 
+ string answer = menu.UserChoice();
+
+ switch(answer)
+{
+
+    case "AddCustomer":
+    menu = new AddCustomerMenu();
+    break;
+
+    case "SearchCustomer":
+    menu = new SearchCustomer();
+    break;
+  case "MainMenu":
+  menu = new MainMenu();
+  break;
+  case "Exit":
+  repeating = false;
+  break;
+  default:
+  Console.WriteLine(" Page does not exist");
+  break;
+
+
+}
+}
